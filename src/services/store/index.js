6d9781ee9a777073826eda0c2b43a205aa59f206
AndreMiller97 <andre.miller97@hotmail.com>
2023-03-15 10:17:42 -0300

    feat: implement Persist Store

--- src/services/store/index.js
+++ src/services/store/index.js
@@ -1,8 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userSlice  from './slices/user'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import userSlice from './slices/user'
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const rootReducer = combineReducers({
+  user: userSlice
+})
+
+const persisteReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice
-  },
+  reducer: persisteReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 })
+
+setupListeners(store.dispatch)
+
+export const persistor = persistStore(store)
